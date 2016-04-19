7cefb97a4cc190fb706d6c3f1d135efedf22d74d
Matt Comi <mattcomi@gmail.com>
2016-04-19 13:34:47 +0800

    Added files via upload

---
+++ ReflectedStringConvertible.swift
@@ -0,0 +1,27 @@
+//  Copyright © 2016 Matt Comi. All rights reserved.
+
+public protocol ReflectedStringConvertible : CustomStringConvertible { }
+
+extension ReflectedStringConvertible {
+  public var description: String {
+    let mirror = Mirror(reflecting: self)
+    
+    var str = "\(mirror.subjectType)("
+    var first = true
+    for (label, value) in mirror.children {
+      if let label = label {
+        if first {
+          first = false
+        } else {
+          str += ", "
+        }
+        str += label
+        str += ": "
+        str += "\(value)"
+      }
+    }
+    str += ")"
+    
+    return str
+  }
+}
