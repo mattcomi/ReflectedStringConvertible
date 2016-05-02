44f72afacd4f05718bcad45e28f53c03e734a3ea
mattcomi <mattcomi@gmail.com>
2016-05-03 01:51:21 +0800

    Renamed scheme in travis.sh from Mac to OSX

--- travis.sh
+++ travis.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-xctool -project ReflectedStringConvertible.xcodeproj -scheme "ReflectedStringConvertible Mac" test
+xctool -project ReflectedStringConvertible.xcodeproj -scheme "ReflectedStringConvertible OSX" test
 xctool -project ReflectedStringConvertible.xcodeproj -scheme "ReflectedStringConvertible iOS" -sdk iphonesimulator9.3 -destination "platform=iOS Simulator,name=iPhone 6" test
